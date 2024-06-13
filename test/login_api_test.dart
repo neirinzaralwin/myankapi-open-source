59793ebc4d8608a8c493257f2f897d484e5bba09
neirinzaralwin <zaralwinneirin@gmail.com>
2024-06-13 22:44:55 +0700

    follow up

--- test/login_api_test.dart
+++ test/login_api_test.dart
@@ -4,8 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:myankapi/data/auth/login/login_req_model.dart';
-import 'package:myankapi/repositories/auth/auth_repository.dart';
-import 'package:myankapi/repositories/singeleton_repositories.dart';
 
 @GenerateNiceMocks([
   // Add the class name of the model you want to generate mocks for
